24496c0e0f9b71275f4fd410682f9e0cdb7e5563
Daniel Sobiech <sobiech151205@gmail.com>
2023-09-15 10:22:58 +0200

    komentarz

---
+++ szkola/gryko/tstr7/funkcje.js
@@ -0,0 +1,15 @@
+//1
+function greet(pr, n){
+    console.log(`Good ${pr} ${n}`);
+}
+
+greet('morning', 'daniel');
+
+//2
+function mean(x,z){
+    console.log((x+z)/2)
+}
+
+mean(12,231);
+mean(35,45);
+mean(592,86);
