280ad75a17a97c3c18aafe3521a3955034eacc69
Daniel Sobiech <sobiech151205@gmail.com>
2023-09-18 13:16:12 +0200

    szkola_18.09.23

--- szkola/gryko/tstr7/funkcje.js
+++ szkola/gryko/tstr7/funkcje.js
@@ -1,15 +1,29 @@
 //1
-function greet(pr, n){
-    console.log(`Good ${pr} ${n}`);
-}
+const suma = (a,b) => {return a+b;}
 
-greet('morning', 'daniel');
+console.log(suma(32,12));
 
 //2
-function mean(x,z){
-    console.log((x+z)/2)
-}
+const kwadrat = (a) => {console.log(a*a);}
 
-mean(12,231);
-mean(35,45);
-mean(592,86);
+kwadrat(23);
+
+//3
+const wypisz = () => {console.log("hello world");}
+
+wypisz();
+
+//4
+const name = () => {console.log("Daniel Sobiech");}
+
+name();
+
+//5
+const przekatna = (a) => {return (a*1.414);}
+
+console.log(przekatna(4));
+
+//6
+const podnies = (a,b) => {console.log(a**b);}
+
+podnies(3,5);
