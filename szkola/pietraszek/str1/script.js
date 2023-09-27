e2edaf9700e7fb2d688d5941325645f768fec1c2
Daniel Sobiech <sobiech151205@gmail.com>
2023-09-27 14:59:13 +0200

    szkola_27.09.23

---
+++ szkola/pietraszek/str1/script.js
@@ -0,0 +1,16 @@
+function suma(){
+    var a = document.getElementById("a");
+    var b = document.getElementById("b");
+    var c = document.getElementById("c");
+
+    alert(Number(a) + Number(b) + Number(c));
+}
+
+function srednia(){
+    var a = document.getElementById("a");
+    var b = document.getElementById("b");
+    var c = document.getElementById("c");
+
+    alert((Number(a) + Number(b) + Number(c)) / 3);
+}
+
