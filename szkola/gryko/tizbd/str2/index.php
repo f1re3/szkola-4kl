11ac32e3fc851f43220ef0fe70b7276596bd98f2
Daniel Sobiech <sobiech151205@gmail.com>
2023-12-07 14:58:24 +0100

    szkola_7.12.2023

--- szkola/gryko/tizbd/str2/index.php
+++ szkola/gryko/tizbd/str2/index.php
@@ -8,51 +8,52 @@
 <body>
 
     <h1>raporty</h1>
-<!-- zapytania do bazy -->
-    
-<!-- Za pomocą narzędzia XAMPP Control Panel uruchom usługę Apache i MySQL. Przejdź następnie do strony http://localhost i wybierz narzędzie phpMyAdmin. Wykonaj następujące czynności: -->
+<?php
+// 1.  Stwórz bazę danych o nazwie Filmoteka.
+// create database filmoteka;
+
+// 2. Zaimportuj do bazy plik baza_zad3.sql rozpakowany z archiwum zad3.zip. Hasło Egz2016@
+//--
+
+// 3. Zapoznaj się ze strukturą bazy danych. Składają się na nią trzy tabele: filmy, rezyserzy oraz recenzje. Każda z nich ma klucz główny rozpoczynający się od „ID”, np. IDFilm. Tabela egzemplarze ma ponadto klucz obcy RezyserIDoraz recenzja id. Referują one kolejno do kluczy głównych tabel rezyszerzy oraz recenzje.
+//--
+
+// 4. Korzystając z zakładki SQL, przygotuj zapytania do bazy i wykonaj je. Kwerendy zapisz w pliku kwerendy.txt w podfolderze baza. Lista kwerend do zdefiniowania:
+
+// a) Zwrócenie tytułów filmów, dla których gatunek to SF,
+// SELECT tytul FROM filmy WHERE gatunek="SF";
+
+// b) Zwrócenie wszystkich tytułów filmów wraz z nazwiskami reżyserów,
+// SELECT tytul, nazwisko FROM filmy INNER JOIN rezyserzy ON filmy.rezyserid=rezyserzy.idrezyser;
+
+// c) Zwrócenie filmów, dla których ocena w recenzji jest równa 4, 
+// SELECT filmy.* FROM filmy INNER JOIN recenzje ON filmy.recenzjaid=recenzje.idrecenzja WHERE ocena='4';
+
+// d) Dodanie rekordu do tabeli rezyserzy z danymi: ID – 4, Andrzej Wajda
+//INSERT INTO `rezyserzy` (`IDRezyser`, `imie`, `nazwisko`) VALUES
+//(4, 'Andrzej', 'Wajda');
+
+
+
+// 5. Wykonaj zrzuty ekranowe z wynikami działania kwerend a, b, c. Na zrzucie powinny być widoczne zwrócone rekordy. Zrzuty zapisz w podfolderze baza.
 
  
 
-<!-- 1.  Stwórz bazę danych o nazwie Filmoteka. -->
+ 
 
-<!-- 2. Zaimportuj do bazy plik baza_zad3.sql rozpakowany z archiwum zad3.zip. Hasło Egz2016@ -->
-
-
-<!-- 3. Zapoznaj się ze strukturą bazy danych. Składają się na nią trzy tabele: filmy, rezyserzy oraz recenzje. Każda z nich ma klucz główny rozpoczynający się od „ID”, np. IDFilm. Tabela egzemplarze ma ponadto klucz obcy RezyserIDoraz recenzja id. Referują one kolejno do kluczy głównych tabel rezyszerzy oraz recenzje. -->
-
-
-<!-- 4. Korzystając z zakładki SQL, przygotuj zapytania do bazy i wykonaj je. Kwerendy zapisz w pliku kwerendy.txt w podfolderze baza. Lista kwerend do zdefiniowania: -->
+// 6. Wyeksportuj bazę do pliku filmoteka.sql i umieść go w podfolderze baza.
+ 
 
  
 
-<!-- a) Zwrócenie tytułów filmów, dla których gatunek to SF, -->
+// Wyświetlenie raportów
+
+// 7 Wyświetl wynik działania kwerendy a. w postaci listy numerowanej
 
 
-<!-- b) Zwrócenie wszystkich tytułów filmów wraz z nazwiskami reżyserów, -->
+// 8. Wyświetl wynik działania kwerendy b. w postaci listy wypunktowanej, tytuł powinien być pogrubiony
 
-<!-- c) Zwrócenie filmów, dla których ocena w recenzji jest równa 4,  -->
-
-<!-- d) Dodanie rekordu do tabeli rezyserzy z danymi: ID – 4, Andrzej Wajda -->
-
-
-<!-- 5. Wykonaj zrzuty ekranowe z wynikami działania kwerend a, b, c. Na zrzucie powinny być widoczne zwrócone rekordy. Zrzuty zapisz w podfolderze baza. -->
-
-
-
-
-
-<!-- 6. Wyeksportuj bazę do pliku filmoteka.sql i umieść go w podfolderze baza. -->
- 
-
-
-
-<!-- Wyświetlenie raportów -->
-
-<!-- 7 Wyświetl wynik działania kwerendy a. w postaci listy numerowanej -->
-
-<!-- 8. Wyświetl wynik działania kwerendy b. w postaci listy wypunktowanej, tytuł powinien być pogrubiony -->
-
-<!-- 9. Wyświetl wynik działania kwerendy c. w postaci tabeli, każde pole w osobnej komórce -->
+// 9. Wyświetl wynik działania kwerendy c. w postaci tabeli, każde pole w osobnej komórce
+?>
 </body>
 </html>
---
+++ szkola/gryko/tsiai/tstr18/srypt.js
@@ -0,0 +1,14 @@
+const miniaturka = document.querySelector('#top img')
+const obraz = document.querySelector('#bottom img')
+
+miniaturka.addEventListener('click', (event)=>{
+    obraz.src=''
+})
+
+miniaturka.addEventListener('mouseover', (event)=>{
+
+})
+
+miniaturka.addEventListener('mouseleave', (event)=>{
+
+})
