0f5ea907787f54d46bd21fdf23b75780b00748c8
Daniel Sobiech <sobiech151205@gmail.com>
2023-12-07 11:36:38 +0100

    szkola

---
+++ szkola/gryko/tizbd/str2/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>raporty</h1>
+<!-- zapytania do bazy -->
+    
+<!-- Za pomocą narzędzia XAMPP Control Panel uruchom usługę Apache i MySQL. Przejdź następnie do strony http://localhost i wybierz narzędzie phpMyAdmin. Wykonaj następujące czynności: -->
+
+ 
+
+<!-- 1.  Stwórz bazę danych o nazwie Filmoteka. -->
+
+<!-- 2. Zaimportuj do bazy plik baza_zad3.sql rozpakowany z archiwum zad3.zip. Hasło Egz2016@ -->
+
+
+<!-- 3. Zapoznaj się ze strukturą bazy danych. Składają się na nią trzy tabele: filmy, rezyserzy oraz recenzje. Każda z nich ma klucz główny rozpoczynający się od „ID”, np. IDFilm. Tabela egzemplarze ma ponadto klucz obcy RezyserIDoraz recenzja id. Referują one kolejno do kluczy głównych tabel rezyszerzy oraz recenzje. -->
+
+
+<!-- 4. Korzystając z zakładki SQL, przygotuj zapytania do bazy i wykonaj je. Kwerendy zapisz w pliku kwerendy.txt w podfolderze baza. Lista kwerend do zdefiniowania: -->
+
+ 
+
+<!-- a) Zwrócenie tytułów filmów, dla których gatunek to SF, -->
+
+
+<!-- b) Zwrócenie wszystkich tytułów filmów wraz z nazwiskami reżyserów, -->
+
+<!-- c) Zwrócenie filmów, dla których ocena w recenzji jest równa 4,  -->
+
+<!-- d) Dodanie rekordu do tabeli rezyserzy z danymi: ID – 4, Andrzej Wajda -->
+
+
+<!-- 5. Wykonaj zrzuty ekranowe z wynikami działania kwerend a, b, c. Na zrzucie powinny być widoczne zwrócone rekordy. Zrzuty zapisz w podfolderze baza. -->
+
+
+
+
+
+<!-- 6. Wyeksportuj bazę do pliku filmoteka.sql i umieść go w podfolderze baza. -->
+ 
+
+
+
+<!-- Wyświetlenie raportów -->
+
+<!-- 7 Wyświetl wynik działania kwerendy a. w postaci listy numerowanej -->
+
+<!-- 8. Wyświetl wynik działania kwerendy b. w postaci listy wypunktowanej, tytuł powinien być pogrubiony -->
+
+<!-- 9. Wyświetl wynik działania kwerendy c. w postaci tabeli, każde pole w osobnej komórce -->
+</body>
+</html>
