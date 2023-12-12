c4f50c661bbb19e6bca33a5dbdb0dce762139a61
Daniel Sobiech <sobiech151205@gmail.com>
2023-12-12 12:25:28 +0100

    szkola

--- szkola/gryko/tsiai/tstr18/srypt.js
+++ szkola/gryko/tsiai/tstr18/srypt.js
@@ -1,14 +1,6 @@
-const miniaturka = document.querySelector('#top img')
+const tabminiaturka = document.querySelectorall('#top img')
 const obraz = document.querySelector('#bottom img')
 
-miniaturka.addEventListener('click', (event)=>{
-    obraz.src=''
-})
-
-miniaturka.addEventListener('mouseover', (event)=>{
-
-})
-
-miniaturka.addEventListener('mouseleave', (event)=>{
-
-})
+for(let miniaturka of tabminiaturka){
+    miniaturka.add
+}
