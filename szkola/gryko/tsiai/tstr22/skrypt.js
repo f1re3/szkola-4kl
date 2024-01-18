8076bc70cf8d193c612f1cf876962e0dc99db0a3
Daniel Sobiech <sobiech151205@gmail.com>
2024-01-18 11:35:58 +0100

    szkola_18.1.24

--- szkola/gryko/tsiai/tstr22/skrypt.js
+++ szkola/gryko/tsiai/tstr22/skrypt.js
@@ -4,10 +4,23 @@ const kod = document.getElementById('kod')
 const kodZ = document.getElementById('kod-zle')
 const adres = document.getElementById('adres')
 const adresZ = document.getElementById('adres-zle')
+const mail = document.getElementById('e-mail')
 const mailZ = document.getElementById('e-mail-zle')
-const result = document.querySelector('p')
+const result = document.querySelector('h4')
 const btnS = document.querySelector('button')
 
-btnS.addEventListener('click', (event)={
-    
+btnS.addEventListener('click', (e)=>{
+    e.preventDefault()
+
+    const wyrM = /^\w{4,}@staszic.eu$/i
+
+    if(nazw.value.length < 3){
+        nazwZ.innerHTML='za malo znakow'
+        nazw.style.backgroundColor='red'
+    }
+    else
+        result.innerHTML=nazw.value+'<br>'
+
+    if(mail.value)
+        mailZ.innerHTML
 })
