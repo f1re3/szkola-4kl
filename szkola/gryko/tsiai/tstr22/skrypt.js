fcae34d7acf9fa59ff4db7d744f1d477789a807d
Daniel Sobiech <sobiech151205@gmail.com>
2024-01-22 08:46:09 +0100

    szkola_22.1.24

--- szkola/gryko/tsiai/tstr22/skrypt.js
+++ szkola/gryko/tsiai/tstr22/skrypt.js
@@ -13,14 +13,47 @@ btnS.addEventListener('click', (e)=>{
     e.preventDefault()
 
     const wyrM = /^\w{4,}@staszic.eu$/i
+    const wyrK = /^[0-9]{2}-[0-9]{3}$/
+    const wyrA = /^[a-z]{3,} .*[0-9]$/i
 
     if(nazw.value.length < 3){
         nazwZ.innerHTML='za malo znakow'
-        nazw.style.backgroundColor='red'
+        // nazw.style.backgroundColor='red'
+        nazw.classList.add('error')
     }
-    else
+    else{
         result.innerHTML=nazw.value+'<br>'
+        nazw.classList.remove('error')
+        nazwZ.innerHTML=''
+    }
 
-    if(mail.value)
-        mailZ.innerHTML
+    if(!wyrM.test(mail.value)){
+        mailZ.innerHTML='Adres Domeny staszic.eu'
+        mail.classList.add('error')
+    }
+    else{
+        result.innerHTML+=mail.value+'<br>'
+        mail.classList.remove('error')
+        mailZ.innerHTML=''
+    }
+
+    if(!wyrK.test(kod.value)){
+        kodZ.innerHTML='zly pod pocztowy'
+        kod.classList.add('error')
+    }
+    else{
+        result.innerHTML+=kod.value+'<br>'
+        kod.classList.remove('error')
+        kodZ.innerHTML=''
+    }
+
+    if(!wyrA.test(adres.value)){
+        adresZ.innerHTML='zly adres'
+        adres.classList.add('error')
+    }
+    else{
+        result.innerHTML+=adres.value
+        adres.classList.remove('error')
+        adresZ.innerHTML=''
+    }
 })
