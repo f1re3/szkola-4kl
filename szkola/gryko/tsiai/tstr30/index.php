359d9ab7b9cbaf584eca32ef416ce3160d755e40
Daniel Sobiech <sobiech151205@gmail.com>
2024-04-23 13:03:37 +0200

    szkola_23.04

--- szkola/gryko/tsiai/tstr30/index.php
+++ szkola/gryko/tsiai/tstr30/index.php
@@ -6,6 +6,26 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+    //4 zadanie funkcja gdzie za zmienna ustawia sie nazwe i potem wywoluje sie nazwe wiersza
+        $plik = fopen("wiersze/jeden.txt", "r");
+        $linijka = fgets($plik);
+        echo "<h1>$linijka</h1>";
+        $linijka = fgets($plik);
+        echo "<h2>$linijka</h2>";
+        while(!feof($plik)){
+            $linijka = fgets($plik);
+            $linijka = nl2br($linijka);
+            echo $linijka;
+        }
+        fclose($plik);
+
+        $zawartosc = scandir("wiersze");
+        foreach ($zawartosc as $key) {
+            if(is_file("wiersze/$key"))
+                echo "<p>$key</p>";
+        }
+        
+    ?>
 </body>
 </html>
